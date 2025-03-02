337f580a4d59eb34885a64c1ed69cb36647e5e8c
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2025-03-02 12:12:04 +0300

    my post

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountain",
         "title": "sms2.PNG",
@@ -12,6 +12,10 @@ posts = [
         "title": "Mountain Hiking",
         "excerpt": "There's nothing Like the views you get when hiking in the Mountain! And I wasn't even prepared for what happend whilst I was enjoying the view!",
         "content": """
+                    At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
+                    
+                    At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
+                    
                     At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
                     """
     },
@@ -24,6 +28,10 @@ posts = [
         "excerpt": "There's nothing Like the views you get when hiking in the Mountain! And I wasn't even prepared for what happend whilst I was enjoying the view!",
         "content": """
                     At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
+                    
+                    At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
+                    
+                    At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
                     """
     },
     {
@@ -35,6 +43,10 @@ posts = [
         "excerpt": "There's nothing Like the views you get when hiking in the Mountain! And I wasn't even prepared for what happend whilst I was enjoying the view!",
         "content": """
                     At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
+                    
+                    At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
+                    
+                    At the bottom right of VS Code, ensure that the language mode is HTML or another supported language. If it shows something else like Plain Text, click on it and change it to HTML
                     """
     }
 ]
@@ -44,16 +56,22 @@ def get_date(post):
     helper function for getting the sorted date.
     """
     # return post.get('date') or
-    return post['post']
+    return post['date']
+    # return post.date
 
 # Create your views here.
 
 def starting_page(request):
     """Renders the home page of the blog.
     """
-    sorted_posts = posts.sort(key=get_date)
+    # sorted_posts = posts.sort(key=get_date)
+    sorted_posts = sorted(all_posts, key=get_date)
     latest_posts = sorted_posts[-3:]
-    return render(request, "blog/index.html", {"recent_posts": latest_posts})  # blog/index.html is b/c template part is looked by django by default. so we don't say templates/blog/index.html
+    return render(request, "blog/index.html", {"posts": latest_posts})  # blog/index.html is b/c template part is looked by django by default. so we don't say templates/blog/index.html
+
+    # or like below
+    # posts = BlogPost.objects.all().order_by("-date")[:3]  # Get latest 3 posts
+    #     return render(request, "blog/index.html", {"posts": posts})
 
 def posts(request):
     """For one post
